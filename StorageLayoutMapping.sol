de4e00e8714bf2ce021a3e70e5aedb88e2663ff9
NocturnusCoder <108350022+NocturnusCoder@users.noreply.github.com>
2023-07-05 14:08:54 +0300

    Create StorageLayoutMapping.sol
    written with the help of https://github.com/jesperkristensen58/solidity_training_cryptojesperk/blob/main/1_mappings/2_mapping.sol

---
+++ StorageLayoutMapping.sol
@@ -0,0 +1,31 @@
+contract StorageLayoutMapping {
+    uint x = 2;
+    mapping(string => uint) stringmapping;
+
+
+    function addToStringMapping(string memory key, uint value) public {
+        stringmapping[key] = value;
+    }
+
+
+    // HELPER TO READ FROM STORAGE SLOTS
+    function readStorageSlot(uint256 i) public view returns (bytes32 content) {
+        assembly {
+            content := sload(i)
+        }
+    }
+
+
+    // HELPER TO GET THE SLOT INDEX OF A MAPPING'S VALUE UNDER IT'S GIVEN KEY
+    function getLocationOfStringMapping(uint mappingSlot, string memory key) public pure returns (bytes32 slot, bytes memory, bytes memory) {
+        // mappingSlot: the slot that the mapping itself sits in -> here: it's slot 2
+        // slot: the slot that the value will be sitting in, e.g.: m[key] = value --> value will sit in "slot."
+        
+        return (keccak256(abi.encodePacked(key,mappingSlot)), abi.encode(key, mappingSlot), abi.encodePacked(key,mappingSlot));
+        // return keccak256(abi.encode(key,mappingSlot)); this will not give the correct value
+               
+        // abi.encodePacked of mappingslot=1 and key="a", value=13 => 
+        // uint(keccak256) of above number => 0xb5cafab5b83d18303877bb912b2d66ca18ab7390cfd9be8a2e66cc5096e0ea02
+        // use this number as input to function readStorageSlot
+    }
+}
